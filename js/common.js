0256a05907a7dc4134359bb3a7757886b3c5e921
ITkah <ilatkacenko132@gmail.com>
2020-03-17 12:40:54 +0200

    add-new-section

--- js/common.js
+++ js/common.js
@@ -1,5 +1,60 @@
 $(document).ready(function() {
 
+    $(".call-menu").on("click", function(){
+        $("nav").slideToggle(200);
+    });
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 20){
+            $('header').addClass("fixed-header");
+        }
+        else{
+            $('header').removeClass("fixed-header");
+        }
+    });
+
+    $('.single').slick({
+        dots: true,
+        arrows: false,
+        fade: true,
+        cssEase: 'linear',
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+
+    $('.responsive').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 3,
+        margin: 10,
+        slidesToScroll: 3,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
+
     $('.multiple-items').slick({
         infinite: true,
         slidesToShow: 2,
@@ -8,8 +63,5 @@ $(document).ready(function() {
         arrows: false
     });
 
-    $(".call-menu").on("click", function(){
-        $("nav").slideToggle(200);
-    });
 
 });
